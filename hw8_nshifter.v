244a9f3cf85d6a6d849d3394eb40d6fd91c346da
chanho <chanho960122@gmail.com>
2019-11-22 19:13:38 +0900

    update tracked files

--- hw8_nshifter.v
+++
@@ -1,9 +0,0 @@
-module hw8_nshifter(din, select, dout);
-	parameter n = 1;
-	input select;
-	input [31:0] din;
-	output [31:0] dout;
-	wire [31:0] d;
-	
-	assign dout[31:0] = select ? din[31 - n:0] : din[31:0];
-endmodule
